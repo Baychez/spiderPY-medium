01e894dc73d0dbae5eb97ce315c326e5269d8702
icarus <ellishogan95@gmail.com>
2019-09-27 23:22:57 -0400

    loop is wrong, it iterates over everything not just the new things

--- spider.py
+++ spider.py
@@ -7,14 +7,16 @@ page = requests.get("https://medium.com")
 
 body = bs4(page.text, "html.parser")
 links = {a['href'] for a in body.find_all("a")}
-
-for i in links.copy():
-    try: 
-        for j in bs4(requests.get(i).content, 'lxml').find_all("a"):
-            links.add(j['href'])
-    except: 
-        pass
-
+try:
+    for r in range(int(sys.argv[1])):
+        for i in links.copy():
+            try: 
+                for j in bs4(requests.get(i).content, 'lxml').find_all("a"):
+                    links.add(j['href'])
+            except: 
+                pass
+except: 
+    pass
 with open('storys.txt', 'w') as s:
     for i in links: 
         if re.search(r'\-+\d+\-+\d+\-+', i):

630f2747f0cc2a344a71b832d56103264729a606
icarus <ellishogan95@gmail.com>
2019-09-29 23:31:51 -0400

    eeh

--- spider.py
+++ spider.py
@@ -9,23 +9,25 @@ body = bs4(page.text, "html.parser")
 links = {a['href'] for a in body.find_all("a")}
 old_links = []
 try:
-    for r in range(int(sys.argv[1])):
-        for i in links.copy():
-            if i not in old_links and re.match('http', i):
-                old_links.append(i)
-                for j in bs4(requests.get(i).content, 'lxml').find_all("a"):
-                    try:
-                        links.add(j['href'])
-                    except KeyError:
-                        pass
-            else:
-                print(i)
+	for r in range(int(sys.argv[1])):
+		for i in links.copy():
+			if i not in old_links and re.match('http', i):
+				old_links.append(i)
+				for j in bs4(requests.get(i).content, 'lxml').find_all("a"):
+					try:
+						links.add(j['href'])
+					except KeyError:
+						pass
+			else:
+				print(i)
 except IndexError: 
-    pass
+	pass
+
+	
 with open('links.txt', 'w') as s:
-    for i in links: 
-        s.write(i + '\n')
+	for i in links: 
+		s.write(i + '\n')
 with open('storys.txt', 'w') as s:
-    for i in links: 
-        if re.search(r'\-+\d+\-+\d+\-+', i):
-            s.write(i + '\n')
+	for i in links: 
+		if re.search(r'\-+\d+\-+\d+\-+', i):
+			s.write(i + '\n')
